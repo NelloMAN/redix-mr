c7bcce37e7141198cc432eaa90f84e8eb000a433
ANIELLO DI FALCO <aniello.difalco@gmail.com>
2022-03-30 17:31:46 +0200

    - Logo redix

--- src/LoginPanel.js
+++ src/LoginPanel.js
@@ -1,14 +1,15 @@
 /*import logo from './logo.svg';
 import './App.css';*/
 import './LoginPanel.css';
+import logo_redix from './img/logo_redix.svg';
 
 function LoginPanel() {
   return (
-    <div className="container-fluid" style={{"height":"100vh"}}>
-        <div className="LoginPanel h-50">
-            <div className="row h-75">
-                <div className="col-sm-4">
-                    Logo
+    <div className="container-fluid d-flex align-items-center justify-content-center w-100 p-0" style={{"height":"100vh"}}>
+        <div className="LoginPanel h-50 w-100 p-0">
+            <div className="row h-100 d-flex align-items-center">
+                <div className="col-sm-4 d-flex align-items-center justify-content-center">
+                    <img src={logo_redix} className='img-fluid w-50 h-auto'></img>
                 </div>
                 <div className="col-sm-4">
                     Form
@@ -18,8 +19,8 @@ function LoginPanel() {
                 </div>
             </div>
             <div className="row">
-                <div className="col-sm-12 h-25">
-                    <p className="text-end">Versione 1.0</p>
+                <div className="col-sm-12">
+                    <p className="text-end m-0">Versione 1.0</p>
                 </div>
             </div>
         </div>
