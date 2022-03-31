b15d43f6527a24ef2bd5130926f7fd9feb17d5ff
ANIELLO DI FALCO <aniello.difalco@gmail.com>
2022-03-31 18:11:26 +0200

    Fix larghezza root/loginPanel

--- src/LoginPanel.js
+++ src/LoginPanel.js
@@ -6,17 +6,15 @@ import logo_redix from './img/logo_redix.svg';
 function LoginPanel() {
   return (
     <div className="container-fluid d-flex align-items-center justify-content-center w-100 p-0" style={{"height":"100vh"}}>
-        <div className="LoginPanel h-50 w-100 p-0">
-            <div className="row h-100 d-flex align-items-center p-0">
-                <div className="col-sm-4 d-flex align-items-center justify-content-center p-0">
-                    <img src={logo_redix} alt="logo_redix" className="img-fluid w-50 h-auto"></img>
-                </div>
-                <div className="col-sm-4 p-0">
-                    Form
-                </div>
-                <div className="col-sm-4 p-0">
-                    EmptySpace
-                </div>
+        <div className="LoginPanel row d-flex align-items-center h-50 w-100">
+            <div className="col-sm-4 d-flex align-items-center justify-content-center">
+                <img src={logo_redix} alt="logo_redix" className="img-fluid w-50 h-auto"></img>
+            </div>
+            <div className="col-sm-4">
+                Form
+            </div>
+            <div className="col-sm-4">
+                EmptySpace
             </div>
         </div>
     </div>
