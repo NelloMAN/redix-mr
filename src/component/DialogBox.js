8f40f80552ad0a96ba39faacec147a1c4ffc59c0
ANIELLO DI FALCO <aniello.difalco@gmail.com>
2022-04-22 14:52:04 +0200

    - Added React-Bootstrap for Modal

--- src/component/DialogBox.js
+++ src/component/DialogBox.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import $ from 'jquery';
-import PropTypes from 'prop-types';
 import './DialogBox.css';
-import { Modal } from 'bootstrap';
+import { Modal, Button } from 'react-bootstrap';
 
 class DialogBox extends React.Component{
 
@@ -13,31 +10,31 @@ class DialogBox extends React.Component{
         this.state = {
             showModal: false
         }
+
+        this.handleClose = this.handleClose.bind(this);
     }
 
     toggleDialogVisibility () {
         this.setState({showModal : !this.state.showModal});
     }
 
+    handleClose () {
+        this.setState({showModal : false});
+    }
+
     render(){
 
         return (
-            <Modal show={this.state.showModal} className="modal fade">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <h4 className="modal-title">Modal title</h4>
-                        </div>
-                        <div className="modal-body">
-                            <p>One fine body&hellip;</p>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary">Save changes</button>
-                        </div>
-                    </div>
-                </div>
+            <Modal show={this.state.showModal} onHide={this.handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Attenzione</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Email o password non corretta</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={this.handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
             </Modal>
           );
     }
